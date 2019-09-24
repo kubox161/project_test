93cb2472cb8fe43b288cb0fec896e8f96a202844
User <user@user.ru>
2019-09-24 16:41:52 +0300

    second commit

--- open_data.py
+++ open_data.py
@@ -1,5 +1,7 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
-data = pd.read_csv("speed_impuls.txt", sep="\t", header=None)
+data = pd.read_csv("speed_impuls.txt", sep="\t", header=None, decimal=',')
 print(data)
-plot(data)
+data.plot()
+plt.show()
