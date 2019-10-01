f3395ff60f22a9f5e303877755f658d64238c784
kubox <kubox61@gmail.com>
2019-10-01 23:25:16 +0300

    6

--- open_data.py
+++ open_data.py
@@ -7,15 +7,26 @@ print (l1)
 #data_zero = pd.DataFrame(0, index=range(data.shape[0]), columns=range(data.shape[1])) # create zero DataFrame
 data_zero = pd.DataFrame(0, index=range(data.shape[0]), columns=range(1)) # create zero DataFrame
 data_2 = pd.concat([data[0],data_zero],axis=1,ignore_index=True)
+t = pd.Series(0, index=range(data.shape[0]))
 for x1 in range(2,l1,1):
     if data.iloc[x1,1]>1 and data.iloc[x1-1,1]<1:
         data_2.iloc[x1,1] = 1
+        t = [t, data.iloc[x1,0]]
+print(t)
+l2 = t.size
+data_speed = pd.DataFrame(0, index=range(data.shape[0]), columns=range(2))
+for l2 in range(l2,7,-1):
+    data_speed.iloc[x2-6,0] = t.iloc[x2]
+    data_speed.iloc[x2 - 6, 1] = 1/(t.iloc[x2]-t.iloc[x2-6])
 #data_zero[0:] = data[0:]
 #data.loc[1,1] = data
 #data_zero['0'] = data['0'].values
 print (data)
 print (data_zero)
 print (data_2)
+print (t)
+print (data_speed)
 data[1].plot()
 data_2[1].plot()
+data_speed.plot()
 plt.show()
