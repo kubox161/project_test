c6873f89f7054379854de56aee27e2af006deab6
User <user@user.ru>
2019-10-01 17:19:45 +0300

    commit number 5

--- open_data.py
+++ open_data.py
@@ -6,16 +6,16 @@ l1 = data.shape[0]    # count row
 print (l1)
 #data_zero = pd.DataFrame(0, index=range(data.shape[0]), columns=range(data.shape[1])) # create zero DataFrame
 data_zero = pd.DataFrame(0, index=range(data.shape[0]), columns=range(1)) # create zero DataFrame
-data_2 = pd.concat([data[0],data_zero],axis=1)
+data_2 = pd.concat([data[0],data_zero],axis=1,ignore_index=True)
 for x1 in range(2,l1,1):
-    if data.iloc[x1,0]>1 and data.iloc[x1-1,1]<1:
+    if data.iloc[x1,1]>1 and data.iloc[x1-1,1]<1:
         data_2.iloc[x1,1] = 1
 #data_zero[0:] = data[0:]
 #data.loc[1,1] = data
 #data_zero['0'] = data['0'].values
-print(data)
-print(data_zero)
+print (data)
+print (data_zero)
 print (data_2)
 data[1].plot()
-data_2
+data_2[1].plot()
 plt.show()
