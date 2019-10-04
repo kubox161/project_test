f13d360ebbaf4c047e80390511dc982edfd0365b
User <user@user.ru>
2019-10-04 16:39:40 +0300

    first commit 7

--- open_data.py
+++ open_data.py
@@ -7,17 +7,22 @@ print (l1)
 #data_zero = pd.DataFrame(0, index=range(data.shape[0]), columns=range(data.shape[1])) # create zero DataFrame
 data_zero = pd.DataFrame(0, index=range(data.shape[0]), columns=range(1)) # create zero DataFrame
 data_2 = pd.concat([data[0],data_zero],axis=1,ignore_index=True)
-t = pd.Series(0, index=range(data.shape[0]))
+#t = pd.Series(0, index=range(0))
+t = pd.Series()
 for x1 in range(2,l1,1):
     if data.iloc[x1,1]>1 and data.iloc[x1-1,1]<1:
         data_2.iloc[x1,1] = 1
-        t = [t, data.iloc[x1,0]]
+        t = t.append(pd.Series([data.iloc[x1,0]]))
+#        t = [t, data.iloc[x1,0]]
 print(t)
 l2 = t.size
-data_speed = pd.DataFrame(0, index=range(data.shape[0]), columns=range(2))
-for l2 in range(l2,7,-1):
-    data_speed.iloc[x2-6,0] = t.iloc[x2]
-    data_speed.iloc[x2 - 6, 1] = 1/(t.iloc[x2]-t.iloc[x2-6])
+print(t.iloc[4])
+data_speed = pd.DataFrame(0, index=range(l2), columns=range(2))
+for x2 in range(l2,7,-1):
+#    data_speed.iloc[x2-6,0] = t[x2]
+#    data_speed.iloc[x2-6,1] = 1/(t[x2]-t[x2-6])
+    data_speed.iloc[x2 - 6, 0] = t.iloc[x2-1]
+    data_speed.iloc[x2-6,1] = 1/(t.iloc[x2-1]- t.iloc[x2-7])
 #data_zero[0:] = data[0:]
 #data.loc[1,1] = data
 #data_zero['0'] = data['0'].values
